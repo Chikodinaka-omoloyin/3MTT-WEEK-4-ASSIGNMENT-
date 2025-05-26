3c8b10e0d34ae7bb84d309ce944d9a7f3ec505f4
chikodinaka-omoloyin <chikodinaka9@gmail.com>
2025-05-26 13:15:26 +0100

    work

--- hello-world-app/src/App.js
+++ hello-world-app/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <h2>Work please</h2>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
